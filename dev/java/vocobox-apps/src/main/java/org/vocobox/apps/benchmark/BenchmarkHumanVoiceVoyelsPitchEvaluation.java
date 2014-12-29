b81f0e47f7a0b05bf85b9e9c25b64453bd5195b1
Martin Pernollet <mpernollet@octo.com>
2014-12-30 00:18:44 +0100

    cleanup

--- dev/java/vocobox-apps/src/main/java/org/vocobox/apps/benchmark/BenchmarkHumanVoiceVoyelsPitchEvaluation.java
+++ dev/java/vocobox-apps/src/main/java/org/vocobox/apps/benchmark/BenchmarkHumanVoiceVoyelsPitchEvaluation.java
@@ -1,95 +1,17 @@
 package org.vocobox.apps.benchmark;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.jzy3d.chart.Chart;
 import org.jzy3d.ui.MultiChartPanel;
-import org.vocobox.apps.benchmark.charts.NotePitchChart;
-import org.vocobox.apps.benchmark.charts.PitchEvalChart;
+import org.vocobox.apps.benchmark.charts.NoteMozaic;
 import org.vocobox.io.datasets.HumanVoiceDataset;
-import org.vocobox.model.note.Note;
-import org.vocobox.model.note.NoteDescriptor;
-import org.vocobox.model.note.NoteDescriptors;
-import org.vocobox.model.note.Voyel;
 
 public class BenchmarkHumanVoiceVoyelsPitchEvaluation {
     public static void main(String[] args) throws Exception {
         HumanVoiceDataset voice = HumanVoiceDataset.VOYELS;
-        List<Note> notes = voice.getNotes();
-        
-        for(Note note: notes){
-            System.out.println(note + " " + note.voyel);
-        }
-        
-        Chart[][] charts = evalCharts(getVoyelMatrix(notes));
+        Chart[][] charts = NoteMozaic.evalChartsVoyels(voice.getNotes());
         ui(voice.getVoyelHeaders(), charts);
-    }
-    
-    public static Chart[][] evalCharts(Note[][] notes) throws Exception {
-        Chart[][] charts = new Chart[2][notes.length];
-        for (int tone = 0; tone < notes.length; tone++) {
-            Note note = notes[tone][0];
-            if(note!=null){
-                charts[0][tone] = chartFrequency(note);
-                charts[1][tone] = chartEval(note, 0.5f);
-            }
-        }    
-
-        return charts;
-    }
-    
-    public static Note[][] getVoyelMatrix(List<Note> notes){
-        Map<Voyel,Integer> counter = getVoyelCount(notes);
-        int max = getMaxCounter(counter);
-        
-        Note[][] matrix = new Note[Voyel.values().length][1];
-        for(Note n: notes){
-            if(matrix[n.voyel.ordinal()][0] == null){
-                matrix[n.voyel.ordinal()][0] = n;
-            }
-        }
-        
-        return matrix;
-    }
-
-    public static int getMaxCounter(Map<Voyel, Integer> counter) {
-        int max = 0;
-        for(Map.Entry<Voyel,Integer> e : counter.entrySet()){
-            if(e.getValue()>max)
-                max = e.getValue();
-        }
-        return max;
-    }
-
-    public static Map<Voyel,Integer> getVoyelCount(List<Note> notes) {
-        Map<Voyel,Integer> counter = new HashMap<Voyel,Integer>();
-        
-        for(Note note: notes){
-            Integer i = counter.get(note.voyel);
-            if(i==null){
-                i = new Integer(1);
-            }
-            else{
-                i = i++;
-            }
-            counter.put(note.voyel, i);
-        }
-        return counter;
-    }
-
-    public static Chart chartEval(Note note, float semitoneDistance) throws Exception {
-        NoteDescriptor descriptor = note.getDescriptor();
-        if("a".equals(descriptor.note.name) || "b".equals(descriptor.note.name)){
-            descriptor = NoteDescriptors.PIANO.getNoteByKey(descriptor.key-12);
-        }
-        return new PitchEvalChart(note, (float)descriptor.frequency, semitoneDistance).getChart();
-    }
-
-    public static Chart chartFrequency(Note n) throws Exception {
-        return new NotePitchChart(n).getChart();
     }
 
     public static void ui(String[] headers, Chart[][] charts) throws IOException {
--- dev/java/vocobox-apps/src/main/java/org/vocobox/apps/benchmark/charts/NoteMozaic.java
+++ dev/java/vocobox-apps/src/main/java/org/vocobox/apps/benchmark/charts/NoteMozaic.java
@@ -2,15 +2,119 @@ package org.vocobox.apps.benchmark.charts;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import org.jzy3d.chart.Chart;
+import org.vocobox.io.datasets.HumanVoiceDataset;
 import org.vocobox.model.note.MAPSNote;
 import org.vocobox.model.note.Note;
+import org.vocobox.model.note.NoteDescriptor;
+import org.vocobox.model.note.NoteDescriptors;
+import org.vocobox.model.note.Voyel;
 
 public class NoteMozaic {
+    
+    public static Chart[][] oneNote(String name, float expected, float semitoneDistance) throws Exception {
+        Note note = HumanVoiceDataset.NOTES.getNote(name);
+        Chart[][] charts = new Chart[2][1];
+        charts[0][0] = chartFrequency(note);
+        charts[0][1] = chartEval(note, semitoneDistance);
+        return charts;
+    }
+
+    
+    public static Chart[][] evalChartsPitch(Note[][] notes, int startAt, float semitoneError) throws Exception {
+        int octaves = notes.length;
+        int semitones = notes[0].length;
+        Chart[][] charts = new Chart[(octaves-startAt)*2][semitones];
+        
+        for (int octave = startAt; octave < octaves; octave++) {
+            for (int tone = 0; tone < notes[octave].length; tone++) {
+                Note note = notes[octave][tone];
+                if(note!=null){
+                    charts[octave-startAt][tone] = chartFrequency(note);
+                    charts[octave-startAt+octaves][tone] = chartEval(note, semitoneError);
+                }
+            }    
+        }
+        return charts;
+    }
+
+    /* */
+    
+    public static Chart[][] evalChartsVoyels(List<Note> voyels) throws Exception {
+        return evalChartsVoyels(getVoyelMatrix(voyels));
+    }
+    
+    public static Chart[][] evalChartsVoyels(Note[][] voyels) throws Exception {
+        Chart[][] charts = new Chart[2][voyels.length];
+        for (int tone = 0; tone < voyels.length; tone++) {
+            Note note = voyels[tone][0];
+            if(note!=null){
+                charts[0][tone] = chartFrequency(note);
+                charts[1][tone] = chartEval(note, 0.5f);
+            }
+        }    
+
+        return charts;
+    }
+    
+    public static Note[][] getVoyelMatrix(List<Note> notes){
+        Map<Voyel,Integer> counter = getVoyelCount(notes);
+        int max = getMaxCounter(counter);
+        
+        Note[][] matrix = new Note[Voyel.values().length][1];
+        for(Note n: notes){
+            if(matrix[n.voyel.ordinal()][0] == null){
+                matrix[n.voyel.ordinal()][0] = n;
+            }
+        }
+        
+        return matrix;
+    }
+
+    public static int getMaxCounter(Map<Voyel, Integer> counter) {
+        int max = 0;
+        for(Map.Entry<Voyel,Integer> e : counter.entrySet()){
+            if(e.getValue()>max)
+                max = e.getValue();
+        }
+        return max;
+    }
+
+    public static Map<Voyel,Integer> getVoyelCount(List<Note> notes) {
+        Map<Voyel,Integer> counter = new HashMap<Voyel,Integer>();
+        
+        for(Note note: notes){
+            Integer i = counter.get(note.voyel);
+            if(i==null){
+                i = new Integer(1);
+            }
+            else{
+                i = i++;
+            }
+            counter.put(note.voyel, i);
+        }
+        return counter;
+    }
+
+    public static Chart chartEval(Note note, float semitoneDistance) throws Exception {
+        NoteDescriptor descriptor = note.getDescriptor();
+        if("a".equals(descriptor.note.name) || "b".equals(descriptor.note.name)){
+            descriptor = NoteDescriptors.PIANO.getNoteByKey(descriptor.key-12);
+        }
+        return new PitchEvalChart(note, (float)descriptor.frequency, semitoneDistance).getChart();
+    }
+
+    public static Chart chartFrequency(Note n) throws Exception {
+        return new NotePitchChart(n).getChart();
+    }
+    
+    /* */
     public static List<Chart> charts(List<Note> notes) throws Exception {
         List<Chart> charts = new ArrayList<Chart>();
         for (Note n : notes)
